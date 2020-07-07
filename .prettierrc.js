540984257037a6f5ac6e26ea2047ae3f6940562c
xeho91 <xeho91@pm.me>
2020-07-07 19:35:49 +0100

    Added Prettier config file

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	// https://prettier.io/docs/en/options.html
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	quoteProps: "as-needed",
+	trailingComma: "es5,"
+	bracketSpacing: true,
+	arrowParens: "always",
+	endOfLine: "lf",
+};
