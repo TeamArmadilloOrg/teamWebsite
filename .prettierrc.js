6da00f66f7f7049ce8fb97b4d47c4b3944cfe163
xeho91 <xeho91@pm.me>
2020-07-07 19:40:32 +0100

    Fixed typo in config

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,7 +5,7 @@ module.exports = {
 	semi: true,
 	singleQuote: false,
 	quoteProps: "as-needed",
-	trailingComma: "es5,"
+	trailingComma: "es5",
 	bracketSpacing: true,
 	arrowParens: "always",
 	endOfLine: "lf",
