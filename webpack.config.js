1c3b27838832b341982bea00d49560981e2e0950
xeho91 <xeho91@pm.me>
2020-07-10 16:20:41 +0100

    🌅[TAWS-26] Added yargs for specifying environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,21 @@
+const yargs = require("yargs");
+
+yargs
+	.option("environment", {
+		alias: "env",
+		describe: "Specify the environment for build",
+		choices: ["development", "testing", "production"],
+		// prettier-ignore
+		demandOption: "Please specify the environment to which you want to build the project from source directory!"
+	})
+	.example([
+		["npm run build development", "Using npm"],
+		["yarn build development", "Using yarn"],
+		["pnpm build development", "Using pnpm"],
+	])
+	.version(false)
+	.help().argv;
+
+const environment = yargs.argv.environment;
+
 module.exports = {};
