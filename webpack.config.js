1f70a976aacbcbe18a91cdd1505c0e14d22b880f
xeho91 <xeho91@pm.me>
2020-07-19 17:52:02 +0100

    🌅[TAWS-26] Configured webpack for MPA & refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var yargs = require("yargs");
 var path = require("path");
+var fs = require("fs");
 
 // yargs config
 yargs
@@ -10,17 +11,20 @@ yargs
 		// prettier-ignore
 		demandOption: "Please specify the environment to which you want to build the project from source directory!"
 	})
-	.example([
-		["npm run build development", "Using npm"],
-		["yarn build development", "Using yarn"],
-		["pnpm build development", "Using pnpm"],
-	])
+	.example(
+		"pnpm build development",
+		'Build project from the source directory in "development" environment'
+	)
 	.version(false)
 	.help().argv;
 
 // Determine the environment set based on the flag passed to command,
 // an argument after --environment or --env
 const environment = yargs.argv.environment;
+
+// webpack plugins
+var { CleanWebpackPlugin } = require("clean-webpack-plugin");
+var HTMLwebpackPlugin = require("html-webpack-plugin");
 
 // a self-invoked function to return a new object with absolute paths
 // to all of the directories inside the project's folder
@@ -35,15 +39,10 @@ var directories = new (function Directories() {
 	this.brand = path.join(this.source, "brand");
 	this.data = path.join(this.source, "data");
 	this.images = path.join(this.source, "images");
-	this.pages = path.join(this.source, "pages");
 	this.scripts = path.join(this.source, "scripts");
 	this.styles = path.join(this.source, "styles");
-
-	this.entryPoint = path.join(this.scripts, "app.js");
+	this.templates = path.join(this.source, "templates");
 })();
-
-var { CleanWebpackPlugin } = require("clean-webpack-plugin");
-var HTMLwebpackPlugin = require("html-webpack-plugin");
 
 var webpackConfig = {
 	// https://webpack.js.org/configuration/mode/
@@ -53,14 +52,25 @@ var webpackConfig = {
 	context: directories.source,
 
 	// https://webpack.js.org/configuration/entry-context/#entry
-	entry: directories.entryPoint,
+	entry: {
+		index: path.join(directories.scripts, "index.js"),
+		...getPagesEntryPoints(),
+	},
 
 	// https://webpack.js.org/configuration/output/
 	output: {
 		// https://webpack.js.org/configuration/output/#outputpath
 		path: directories.output,
 		// https://webpack.js.org/configuration/output/#outputfilename
-		filename: "[name].bundle.js",
+		filename: "assets/scripts/[name].bundle.js",
+	},
+
+	// https://webpack.js.org/configuration/resolve/
+	resolve: {
+		// https://webpack.js.org/configuration/resolve/#resolvealias
+		alias: {
+			"@": directories.source, // root directory of source
+		},
 	},
 
 	// https://webpack.js.org/configuration/module/
@@ -119,7 +129,7 @@ var webpackConfig = {
 						loader: "file-loader",
 						// https://webpack.js.org/loaders/file-loader/#options
 						options: {
-							name: "[path][name].[ext]",
+							name: "assets/[path][name].[ext]",
 						},
 					},
 				],
@@ -135,8 +145,12 @@ var webpackConfig = {
 		new HTMLwebpackPlugin({
 			// https://github.com/jantimon/html-webpack-plugin#options
 			filename: "index.html",
-			template: path.join(directories.source, "index.html"),
+			template: path.join(directories.templates, "index.html"),
+			chunks: ["index"],
+			inject: "head",
 		}),
+
+		...generatePagesTemplatesWithHTMLwebpackPlugin(),
 	],
 
 	// https://webpack.js.org/configuration/dev-server/#devserver
@@ -151,5 +165,45 @@ var webpackConfig = {
 		watchContentBase: true,
 	},
 };
+
+function getPagesEntryPoints() {
+	function convertArrayToObjectWithPaths(EntryPointFilename) {
+		const entryPointBasename = path.basename(EntryPointFilename, ".js");
+		const entryPointFullPath = path.join(
+			directories.scripts,
+			"pages",
+			EntryPointFilename
+		);
+
+		return { [entryPointBasename]: entryPointFullPath };
+	}
+
+	return Object.assign(
+		{},
+		...fs
+			.readdirSync(path.join(directories.scripts, "pages"))
+			.map(convertArrayToObjectWithPaths)
+	);
+}
+
+function generatePagesTemplatesWithHTMLwebpackPlugin() {
+	function generatePageTemplate(templateFilename) {
+		return new HTMLwebpackPlugin({
+			// https://github.com/jantimon/html-webpack-plugin#options
+			filename: templateFilename,
+			template: path.join(
+				directories.templates,
+				"pages",
+				templateFilename
+			),
+			chunks: ["index", path.basename(templateFilename, ".html")],
+			inject: "head",
+		});
+	}
+
+	return fs
+		.readdirSync(path.join(directories.templates, "pages"))
+		.map(generatePageTemplate);
+}
 
 module.exports = webpackConfig;
