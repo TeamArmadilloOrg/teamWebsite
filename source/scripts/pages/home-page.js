06839b7ef6a069be80d08da8a0c8a8d4f49ec3f0
Zachary King <lax0248917@gmail.com>
2020-08-06 23:05:49 -0400

    🚀[TAWS-75] Added template, styles, & script files

---
+++ source/scripts/pages/home-page.js
@@ -0,0 +1,8 @@
+import "@/styles/pages/home-page.css";
+import "@/brand/favicon/favicon.ico";
+
+import template from "@/templates/pages/home-page.html";
+
+window.addEventListener("DOMContentLoaded", function () {
+	document.body.insertAdjacentHTML("beforeend", template);
+});
