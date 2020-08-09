474a9744654c3a2559e0c57bcdfe7dce9244c67b
Zachary King <lax0248917@gmail.com>
2020-08-09 13:06:27 -0400

    :rocket:[TAWS-75] Removes excess duplication of the addEventListener and properly sets the attribute with our image in home.html

--- source/scripts/pages/home.js
+++ source/scripts/pages/home.js
@@ -1,12 +1,8 @@
 import "@/styles/pages/home.css";
 import faviconPath from "@/brand/favicon/favicon.ico";
-
-window.addEvenListener("DOMContentLoaded", function () {
-	document.querySelector("#logo img").src = faviconPath;
-});
-
 import template from "@/templates/pages/home.html";
 
 window.addEventListener("DOMContentLoaded", function () {
 	document.body.insertAdjacentHTML("beforeend", template);
+	document.querySelector("#logo img").setAttribute("src", faviconPath);
 });
