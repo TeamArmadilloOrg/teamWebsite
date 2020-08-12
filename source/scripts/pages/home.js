dbb2b4695455ad8b2c026333b3cd0352c159beee
Marius Hilleke <marius.hilleke@trivadis.com>
2020-08-12 22:20:33 +0200

    :turtle:[TAWS-88] added initial link with twitter icon

--- source/scripts/pages/home.js
+++ source/scripts/pages/home.js
@@ -1,8 +1,11 @@
 import "@/styles/pages/home.css";
 import faviconPath from "@/brand/favicon/favicon.ico";
+import twitterIconPath from "@/brand/twitter_logo.png";
 import template from "@/templates/pages/home.html";
 
 window.addEventListener("DOMContentLoaded", function () {
 	document.body.insertAdjacentHTML("beforeend", template);
 	document.querySelector("#logo img").setAttribute("src", faviconPath);
+	document.querySelector("#twitter a img").setAttribute("src", twitterIconPath);
+	document.querySelector("#twitter a").setAttribute("href", "https://twitter.com/lilly_armadillo");
 });
