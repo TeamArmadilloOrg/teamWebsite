a29360739d12331f66aa18d1ae8fd733031a0003
Marius Hilleke <marius.hilleke@trivadis.com>
2020-08-25 17:49:19 +0200

    :turtle:[TAWS-88] obsolete since twitter-href does not change dynamically
    > furthermore it is already included via the template cf. home.html

--- source/scripts/pages/home.js
+++ source/scripts/pages/home.js
@@ -1,13 +1,8 @@
 import "@/styles/pages/home.css";
 import faviconPath from "@/brand/favicon/favicon.ico";
-import twitterIconPath from "@/brand/twitter_logo.png";
 import template from "@/templates/pages/home.html";
 
 window.addEventListener("DOMContentLoaded", function () {
-	const twitterAccountHref = "https://twitter.com/lilly_armadillo";
 	document.body.insertAdjacentHTML("beforeend", template);
-	document.querySelector("#logo img").setAttribute("src", faviconPath);
-	document.querySelector("#twitter a img").setAttribute("src", twitterIconPath);
-	document.querySelector("#twitter a").setAttribute("href", twitterAccountHref);
 	document.querySelector("#logo").setAttribute("src", faviconPath);
 });
