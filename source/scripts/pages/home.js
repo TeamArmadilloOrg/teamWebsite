de10c3263cce21397c7a12fd2340b1862760b017
Marius Hilleke <marius.hilleke@trivadis.com>
2020-08-31 07:41:59 +0200

    :turtle:[TAWS-97] added template for team-page

--- source/scripts/pages/home.js
+++ source/scripts/pages/home.js
@@ -1,8 +1,40 @@
 import "@/styles/pages/home.css";
 import faviconPath from "@/brand/favicon/favicon.ico";
 import template from "@/templates/pages/home.html";
+import * as Team from "@/scripts/pages/team.js";
 
 window.addEventListener("DOMContentLoaded", function () {
 	document.body.insertAdjacentHTML("beforeend", template);
 	document.querySelector("#logo").setAttribute("src", faviconPath);
+	addCustomEventListener();
 });
+
+function addCustomEventListener() {
+	addTeamLinkNavigation();
+}
+
+function addTeamLinkNavigation() {
+	document
+		.querySelector("#team-link")
+		.addEventListener("click", function renderTeamPage(e) {
+			const main_container = document.querySelector("#main-container");
+			while (main_container.firstChild) {
+				main_container.removeChild(main_container.firstChild);
+			}
+			const teamTemplate = Team.getTemplate();
+			main_container.insertAdjacentHTML("beforeend", teamTemplate);
+		});
+}
+
+//Work in progress
+// why does this immediately invoke the renderTeamPage function although it is just added for the
+// click-event
+// function addCustomEventListener(){
+// 	document.querySelector("#team-link").addEventListener("click", function renderTeamPage(e){
+// 	});
+// }
+
+// function renderTeamPage(){
+// 	const teamTemplate = Team.getTemplate();
+// 	document.querySelector("#main-container").insertAdjacentHTML("beforeend", teamTemplate);
+// }
---
+++ source/scripts/pages/team.js
@@ -0,0 +1,6 @@
+import "@/styles/pages/team.css";
+import template from "@/templates/pages/team.html";
+
+export function getTemplate(){
+	return template;
+}
